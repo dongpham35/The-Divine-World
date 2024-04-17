ac38b307be79ccdffc6d21abe114390a75d20727
dongpham35 <81824832+dongpham35@users.noreply.github.com>
2024-04-18 01:56:18 +0700

    update

---
+++ Assets/Scripts/LogIn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Runtime.Serialization.Json;
+using Assets.Scripts.Models;
+using UnityEngine.UI;
+using TMPro;
+
+public class LogIn : MonoBehaviour
+{
+    public TMP_InputField usernameInputField;
+    public TMP_InputField passwordInputField;
+
+    private string username;
+    private string password;
+    private string URL;
+
+
+    public void SignIn()
+    {
+
+        username = usernameInputField.text;
+        password = passwordInputField.text;
+        URL = "http://192.168.1.7/TheDiVWorld/api/Account?username=" + username;
+        StartCoroutine(SendLoginRequest());
+
+    }
+
+    IEnumerator SendLoginRequest()
+    {
+        using(UnityWebRequest request = UnityWebRequest.Get(URL))
+        {
+            yield return request.SendWebRequest();
+            if(request.isNetworkError || request.isHttpError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                string json = request.downloadHandler.text;
+
+                SimpleJSON.JSONNode stats = SimpleJSON.JSON.Parse(json);
+                string sql_password = stats["password"].ToString().Replace('"',' ').Replace(" ", "");
+                if (password.Equals(sql_password))
+                {
+                    Debug.Log("Dang nhap thanh cong");
+                }
+                else
+                {
+                    Debug.Log("Tên tài khoản hoặc mật khẩu không đúng!");
+                }
+            }
+            request.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/Models/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Models
+{
+    internal class Account
+    {
+        public string username { get; set; }
+        public string avatar { get;set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int gold { get; set; }
+        public int levelID { get; set; }
+        public string @class { get; set; }
+
+        public Account()
+        {
+            this.username = "";
+            this.avatar = "";
+            this.email = "";
+            this.password = "";
+            this.gold = 0;
+            this.levelID = 0;
+            this.@class = "";
+        }
+    }
+}
