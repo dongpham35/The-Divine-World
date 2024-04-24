ebaca11e20edccf1ea1f1212206c380c8d41aac2
dongpham35 <81824832+dongpham35@users.noreply.github.com>
2024-04-24 10:44:26 +0700

    update attack

---
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonPlayerCharacter : MonoBehaviour
+{
+
+    public void ShowCanvas()
+    {
+        Debug.Log("Dag ns chuyen");
+    }
+}
